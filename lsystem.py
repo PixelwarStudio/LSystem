b37ac117d8819e2c1cfaf3485a20ee027ee37d4d
PixelwarStudio <janko.matthes@gmx.net>
2018-03-19 11:36:36 +0100

    add docstrings

--- lsystem.py
+++ lsystem.py
@@ -1,14 +1,23 @@
+"""
+Module for handle lsystems.
+"""
 from random import random
 from functools import reduce
 
 class LSystem(object):
+    """A simple LSystem without extras"""
     def __init__(self, axiom, rules):
         self.axiom = axiom
         self.rules = rules
         self.state = self.axiom
         self.generation = 0
-
+    
     def step(self, steps=1):
+        """Translate the current state into a new state by applying the rules. 
+
+        Args:
+            steps (int): Indicate how many times the rules will be applied.
+        """
         for _ in range(steps):
             new_state = ""
             for symbol in self.state:
@@ -20,9 +29,19 @@ class LSystem(object):
         self.generation += steps
     
     def reset(self):
+        """Reset the state to axiom."""
         self.axiom = self.state
     
     def _get_successor(self, symbol):
+        """Find the proper rule for the symbol.
+
+        Args:
+            symbol (str): The symbol.
+
+        Returns:
+            str: The successor of the symbol.
+        """
+        
         return self.rules[symbol]
 
     def __len__(self):
@@ -38,6 +57,7 @@ class LSystem(object):
         return iter(self.state)
 
 class ProbLSystem(LSystem):
+    """A stochastic LSystem."""
     def __init__(self, axiom, rules):
         super().__init__(axiom, rules)
 
