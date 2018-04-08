6f7bd8d624cb2577fbe83585a9522920b07dce00
PixelwarStudio <janko.matthes@gmx.net>
2018-04-08 15:05:34 +0200

    add koch snowflake as example

---
+++ examples/snowflake.py
@@ -0,0 +1,31 @@
+from lsystem import Var, Const, LSystem
+from symbols import Forward, Rotate as Rot
+from PillowTurtle import Turtle
+from PIL import Image
+
+class F(Forward):
+    def __init__(self):
+        super().__init__(2)
+
+    def replace(self):
+        return (F(), Rot(60), F(), Rot(-120), F(), Rot(60), F())
+    
+    def interpret(self, args):
+        turtle = args[0]
+        turtle.forward(self.dist)
+
+def main():
+    im = Image.new("RGB", (1000, 1000))
+
+    turtle = Turtle([750, 250], 180)
+
+    snowflake = LSystem((F(), Rot(-120), F(), Rot(-120), F()))
+    snowflake.step(5)
+    snowflake.interpret(turtle)
+
+    turtle.draw(im)
+
+    im.show()
+
+if __name__ == "__main__":
+    main()
